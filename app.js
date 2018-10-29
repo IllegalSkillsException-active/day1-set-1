3f66a1878aa507a1341077e41e9e5dcce51aaa8a
randy sartor <randal.sartor@gmail.com>
2018-10-29 14:56:07 -0400

    finished first exercise

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
+let yob = ''
+
 function createGreeting(name, age){
-    
-    
     let yearOf = yearOfBirth(age); 
-    return `Hi my name is ${name}. I'm ${age} years old. I was born on ${yearOf}.`; 
+
+    if (typeof age !== 'number') {
+        throw new Error("Age must be a number")
     } 
+    
+    if (typeof name !== 'string') {
+        throw new Error("Name must be a string")
+    } 
+    
+    if (name === undefined || age === undefined) {
+        throw new Error("Arguments not valid")
+    }   
+
+    return `Hi my name is ${name}. I'm ${age} years old. I was born in ${yob}.`; 
+} 
     // createGreeting(); 
+
 function yearOfBirth(age){
-    if( age < 0 ){
+    if( age < 0 ) {
         throw new Error("age cannot be negative"); 
         // console.log('please enter a positive number'); 
         return createGreeting();  
+    } else {
+        return yob = 2018 - age;
     }
 }
-    try{
-        // const greeting1 = createGreeting("taylor", 12);
-        // console.log(greeting1); 
-}
-catch(Error){
+    
+
+try {
+    const greeting1 = createGreeting("taylor", 12);
+} catch(Error) { 
     console.error(Error); 
 }
-}
-console.log(createGreeting('taylor', 12)); 
+
+console.log(createGreeting("Randy", 12)); 
